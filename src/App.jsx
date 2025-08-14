fb1397763205f4eb1142bd95a0255454c4547e75
Tolu Abolude <toludavid07@gmail.com>
2025-08-14 13:20:48 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+- import * as React from "react";
++ import * as React from "react";
+- export default function DistanceTimeCalculator() {
++ function DistanceTimeCalculator() {
++   // (code stays the same)
++ }
++ export default DistanceTimeCalculator;
