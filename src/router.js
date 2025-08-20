231165cf906b9b7d6507007e07962c5598785261
Tolu Abolude <toludavid07@gmail.com>
2025-08-20 22:25:32 +0100

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,10 +1,14 @@
-export function useHashRoute(defaultPath = "#/time") {
+import * as React from 'react';
+
+export function useHashRoute(defaultPath = '#/time') {
   const [path, setPath] = React.useState(window.location.hash || defaultPath);
+
   React.useEffect(() => {
     const onHash = () => setPath(window.location.hash || defaultPath);
-    window.addEventListener("hashchange", onHash);
+    window.addEventListener('hashchange', onHash);
     if (!window.location.hash) window.location.hash = defaultPath;
-    return () => window.removeEventListener("hashchange", onHash);
+    return () => window.removeEventListener('hashchange', onHash);
   }, [defaultPath]);
+
   return path;
 }
