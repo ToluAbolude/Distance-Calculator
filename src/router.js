1430afe5d1f26df4378923a6846fdb820f1ae82e
Tolu Abolude <toludavid07@gmail.com>
2025-08-20 21:55:08 +0100

    Create router.js

---
+++ src/router.js
@@ -0,0 +1,10 @@
+export function useHashRoute(defaultPath = "#/time") {
+  const [path, setPath] = React.useState(window.location.hash || defaultPath);
+  React.useEffect(() => {
+    const onHash = () => setPath(window.location.hash || defaultPath);
+    window.addEventListener("hashchange", onHash);
+    if (!window.location.hash) window.location.hash = defaultPath;
+    return () => window.removeEventListener("hashchange", onHash);
+  }, [defaultPath]);
+  return path;
+}
